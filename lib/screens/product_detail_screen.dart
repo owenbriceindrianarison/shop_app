0c00ea0d105cf7435e60d56afe35477ce1527077
Owen <indrianarison.brice@gmail.com>
2020-09-04 15:31:40 +0300

    Product Detail Screen Navigation

---
+++ lib/screens/product_detail_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  static const routeName = '/product-detail';
+
+  @override
+  Widget build(BuildContext context) {
+    final productId = ModalRoute.of(context).settings.arguments as String;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product'),
+      ),
+      body: Container(
+        child: Center(
+          child: Text(productId),
+        ),
+      ),
+    );
+  }
+}

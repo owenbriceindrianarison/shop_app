927c562bbe296fe4a7b6fc179f76cf866e58a637
Owen <indrianarison.brice@gmail.com>
2020-09-11 14:18:28 +0300

    Using Forms @ Form Inputs

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:shop_app/screens/product_detail_screen.dart';
 import 'package:shop_app/screens/cart_screen.dart';
 import 'package:shop_app/screens//orders_screen.dart';
 import 'package:shop_app/screens/user_product_screen.dart';
+import 'package:shop_app/screens/edit_product_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -45,6 +46,7 @@ class MyApp extends StatelessWidget {
           CartScreen.routeName: (ctx) => CartScreen(),
           OrdersScreen.routeName: (ctx) => OrdersScreen(),
           UserProductScreen.routeName: (ctx) => UserProductScreen(),
+          EditProductScreen.routeName: (ctx) => EditProductScreen(),
         },
       ),
     );
--- lib/screens/user_product_screen.dart
+++ lib/screens/user_product_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:shop_app/providers/products.dart';
+import 'package:shop_app/screens/edit_product_screen.dart';
 import 'package:shop_app/widgets/app_drawer.dart';
 import 'package:shop_app/widgets/user_product_item.dart';
 
@@ -15,7 +16,13 @@ class UserProductScreen extends StatelessWidget {
         title: const Text(
           'Your Products',
         ),
-        actions: <Widget>[IconButton(icon: Icon(Icons.add), onPressed: null)],
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () =>
+                Navigator.of(context).pushNamed(EditProductScreen.routeName),
+          ),
+        ],
       ),
       drawer: AppDrawer(),
       body: Padding(
